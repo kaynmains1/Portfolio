b639ece550ec3a0d239fcd382a79e95437e75bc8
Alex <alexeytimonov1@gmail.com>
2024-12-23 20:26:46 +0200

    hide password

--- send_email.py
+++ send_email.py
@@ -5,9 +5,9 @@ import os
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = "lebronlebron321321@gmail.com"
-    password = os.getenv("PASSWORD")
-    receiver = "lebronlebron321321@gmail.com"
+    username = "HERE SHOULD BE EMAIL"
+    password = "HERE SHOULD BE YOUR PASSOWRD"
+    receiver = "HERE SHOULD BE EMAIL"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
